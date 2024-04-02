2c8e11d3714c8c3058050cfe1acbe36ba634ab27
cristianoliveira <contato@cristianoliveira.com.br>
2024-04-02 19:17:20 +0200

    refact: declare packages per system dynamically

--- flake.nix
+++ flake.nix
@@ -1,45 +1,22 @@
 {
   description = "My custom packages for Nix";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }:
-    {
-      # Linux
-      packages.x86_64-linux.ergo = 
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      systemPackages = map (system:
         let
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-          };
+          pkgs = import nixpkgs { inherit system; };
         in
-          pkgs.callPackage ./ergo.nix {};
-
-      packages.aarch64-linux.ergo = {
-        ergo = 
-          let
-            pkgs = import nixpkgs {
-              system = "aarch64-linux";
-            };
-          in
-            pkgs.callPackage ./ergo.nix {};
-      };
-
-      # OSX
-      packages.x86_64-darwin.ergo = {
-        ergo = 
-          let
-            pkgs = import nixpkgs {
-              system = "x86_64-darwin";
-            };
-          in
-            pkgs.callPackage ./ergo.nix {};
-      };
-
-
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.ergo;
-      defaultPackage.aarch64-linux = self.packages.aarch64-linux.ergo;
-      defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.ergo;
-    };
+        {
+          packages."${system}" = {
+            ergo = pkgs.callPackage ./ergo.nix {};
+          };
+        }
+      ) systems;
+    in
+      # Reduce the list of packages of packages into a single attribute set
+      builtins.foldl' (cur: acc: cur // acc) {} systemPackages;
 }

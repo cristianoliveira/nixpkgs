dcd76049fc6f2593b756ac516072d4c28fb3b492
cristianoliveira <contato@cristianoliveira.com.br>
2024-09-13 01:01:51 +0200

    fix: importing the ergo packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,10 +2,10 @@
   let 
     funzzy = pkgs.callPackage ./funzzy.nix {};
     funzzyNightly = pkgs.callPackage ./funzzy-nightly.nix {};
-  in {
+
     ergoProxyNightly = pkgs.callPackage ./ergo-proxy-nightly.nix { };
     ergoProxy = pkgs.callPackage ./ergo-proxy.nix {};
-
+  in {
     # too many aliases
     funzzy = funzzy;
     fzz = funzzy;

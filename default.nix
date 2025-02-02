127850dd4775a0927c3c2dd69a534376dcfe6012
cristianoliveira <contato@cristianoliveira.com.br>
2025-02-02 14:40:19 +0100

    feat: fetch packages from repo

--- default.nix
+++ default.nix
@@ -1,14 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
-  let 
-    funzzy = pkgs.callPackage ./pkgs/funzzy.nix {};
-    funzzyNightly = pkgs.callPackage ./pkgs/funzzy-nightly.nix {};
-  in {
-    # too many aliases
-    funzzy = funzzy;
-    fzz = funzzy;
-    funzzyNightly = funzzyNightly;
-    fzzNightly = funzzyNightly;
-
-    ergoProxyNightly = pkgs.callPackage ./pkgs/ergo-proxy-nightly.nix { };
-    ergoProxy = pkgs.callPackage ./pkgs/ergo-proxy.nix {};
-  }
+{
+  ergoProxyNightly = pkgs.callPackage ./pkgs/ergo-proxy-nightly.nix { };
+  ergoProxy = pkgs.callPackage ./pkgs/ergo-proxy.nix {};
+}
--- flake.nix
+++ flake.nix
@@ -5,18 +5,27 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     utils.url = "github:numtide/flake-utils";
 
+    funzzy.url = "github:cristianoliveira/funzzy";
     sway-setter.url = "github:cristianoliveira/sway-setter";
   };
 
-  outputs = { self, nixpkgs, utils, sway-setter, ... }:
+  outputs = { self, nixpkgs, utils, sway-setter, funzzy, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         srcpkgs = import ./default.nix { inherit pkgs; };
         swaysetterPkgs = import sway-setter { inherit pkgs; };
+        funzzyPkgs = import funzzy { inherit pkgs; };
       in {
         packages = srcpkgs // {
+          # Sway Setter packages
           sway-setter = swaysetterPkgs.default;
+
+          # Funzzy packages
+          funzzy = funzzyPkgs.default;
+          fzz = funzzyPkgs.default;
+          funzzyNightly = funzzyPkgs.nightly;
+          fzzNightly = funzzyPkgs.nightly;
         };
 
         devShells.default = pkgs.callPackage ./shell.nix { inherit pkgs srcpkgs; };

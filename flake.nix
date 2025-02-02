a105418496cb0f92f5c9636558c955337e980cad
cristianoliveira <contato@cristianoliveira.com.br>
2025-02-02 14:27:30 +0100

    feat: adds sway-setter packages

--- flake.nix
+++ flake.nix
@@ -4,15 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     utils.url = "github:numtide/flake-utils";
+
+    sway-setter.url = "github:cristianoliveira/sway-setter";
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
+  outputs = { self, nixpkgs, utils, sway-setter, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         srcpkgs = import ./default.nix { inherit pkgs; };
+        swaysetterPkgs = import sway-setter { inherit pkgs; };
       in {
-        packages = srcpkgs;
+        packages = srcpkgs // {
+          sway-setter = swaysetterPkgs.default;
+        };
 
         devShells.default = pkgs.callPackage ./shell.nix { inherit pkgs srcpkgs; };
     });

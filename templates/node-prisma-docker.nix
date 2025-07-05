7ca0c5c46543caa207a7c78b07859b7daada57ac
cristianoliveira <contato@cristianoliveira.com.br>
2025-07-05 19:27:30 +0200

    feat(nix): add node-prisma-docker template
    Summary:
    This commit introduces a new Nix flake template for Node.js projects using Prisma and Docker, streamlining the setup process for development environments.
    
    Detailed Changes:
     - templates/node-prisma-docker.nix:
       - Added a new file defining a Nix flake with development shells and package configurations for Node.js, Prisma, and Docker.

---
+++ templates/node-prisma-docker.nix
@@ -0,0 +1,34 @@
+
+{
+  description = "Nix flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, utils, ... }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            prisma-engines
+            prisma
+
+            docker
+            docker-compose
+          ];
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          '';
+        };
+
+        packages = {
+          default = pkgs.hello;
+        };
+    });
+}

1a7d94758ea808085c5abb6f4e172524b2b91ca2
cristianoliveira <contato@cristianoliveira.com.br>
2026-04-27 11:19:43 +0200

    chore(pi): bump to 0.70.2

--- pkgs/pi/default.nix
+++ pkgs/pi/default.nix
@@ -1,67 +1,71 @@
-{ pkgs ? import <nixpkgs> {}, ... }: {
-  pi = let
-    version = "0.72.1";
+{ pkgs ? import <nixpkgs> { }, ... }: {
+  pi =
+    let
+      version = "0.72.1";
 
-    # Determine the architecture-specific file
-    archFile = if pkgs.stdenv.isDarwin then
-      if pkgs.stdenv.isAarch64 then "pi-darwin-arm64.tar.gz"
-      else "pi-darwin-x64.tar.gz"
-    else if pkgs.stdenv.isAarch64 then "pi-linux-arm64.tar.gz"
-    else "pi-linux-x64.tar.gz";
+      # Determine the architecture-specific file
+      archFile =
+        if pkgs.stdenv.isDarwin then
+          if pkgs.stdenv.isAarch64 then "pi-darwin-arm64.tar.gz"
+          else "pi-darwin-x64.tar.gz"
+        else if pkgs.stdenv.isAarch64 then "pi-linux-arm64.tar.gz"
+        else "pi-linux-x64.tar.gz";
 
-    # Update sha256 as needed - use empty string "" and nix will tell you the correct one
-    # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-arm64.tar.gz
-    # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-x64.tar.gz
-    # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-arm64.tar.gz
-    # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-x64.tar.gz
-    sha256 = if pkgs.stdenv.isDarwin then
-      if pkgs.stdenv.isAarch64 then "sha256-QLLwJ/wPWBMXBykhvy593+yHHDpOlLc8Odc/wqvF5Rc="
-      else "sha256-WapVtIPNQteAefGjtiRPi667Y+pN7III1y6wvy32mqI="
-    else if pkgs.stdenv.isAarch64 then "sha256-ecTFhXZc/aAWJL3h71KcblUIG83eMFjmTr6P39wUtKg="
-    else "sha256-APq4sfGUFAqMnb0mFLOOmXJs1d3jtbLWB71dqm+FHUA=";
+      # Update sha256 as needed - use empty string "" and nix will tell you the correct one
+      # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-arm64.tar.gz
+      # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-darwin-x64.tar.gz
+      # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-arm64.tar.gz
+      # nix-prefetch-url https://github.com/badlogic/pi-mono/releases/download/v${version}/pi-linux-x64.tar.gz
+      sha256 =
+        if pkgs.stdenv.isDarwin then
+          if pkgs.stdenv.isAarch64 then "sha256-QLLwJ/wPWBMXBykhvy593+yHHDpOlLc8Odc/wqvF5Rc="
+          else "sha256-WapVtIPNQteAefGjtiRPi667Y+pN7III1y6wvy32mqI="
+        else if pkgs.stdenv.isAarch64 then "sha256-ecTFhXZc/aAWJL3h71KcblUIG83eMFjmTr6P39wUtKg="
+        else "sha256-APq4sfGUFAqMnb0mFLOOmXJs1d3jtbLWB71dqm+FHUA=";
 
-    src = pkgs.fetchurl {
-      url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/${archFile}";
-      inherit sha256;
+      src = pkgs.fetchurl {
+        url = "https://github.com/badlogic/pi-mono/releases/download/v${version}/${archFile}";
+        inherit sha256;
+      };
+    in
+    pkgs.stdenv.mkDerivation {
+      pname = "pi";
+      inherit version src;
+
+      nativeBuildInputs = [ pkgs.gnutar pkgs.gzip ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.autoPatchelfHook ];
+
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
+        pkgs.stdenv.cc.cc.lib
+      ];
+
+      sourceRoot = ".";
+
+      unpackPhase = ''
+        runHook preUnpack
+        tar xzf ${src}
+        runHook postUnpack
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        # Copy entire pi directory to share
+        mkdir -p $out/share
+        cp -r pi $out/share/
+        # Create bin symlink
+        mkdir -p $out/bin
+        ln -sf $out/share/pi/pi $out/bin/pi
+        runHook postInstall
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Pi AI coding assistant";
+        homepage = "https://github.com/badlogic/pi-mono";
+        license = licenses.mit;
+        platforms = platforms.unix;
+        maintainers = [ ];
+      };
+
+      passthru.updateScript = ./update.sh;
     };
-  in pkgs.stdenv.mkDerivation {
-    pname = "pi";
-    inherit version src;
-
-    nativeBuildInputs = [ pkgs.gnutar pkgs.gzip ]
-      ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.autoPatchelfHook ];
-
-    buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
-      pkgs.stdenv.cc.cc.lib
-    ];
-
-    sourceRoot = ".";
-
-    unpackPhase = ''
-      runHook preUnpack
-      tar xzf ${src}
-      runHook postUnpack
-    '';
-
-    installPhase = ''
-      runHook preInstall
-      # Copy entire pi directory to share
-      mkdir -p $out/share
-      cp -r pi $out/share/
-      # Create bin symlink
-      mkdir -p $out/bin
-      ln -sf $out/share/pi/pi $out/bin/pi
-      runHook postInstall
-    '';
-
-    meta = with pkgs.lib; {
-      description = "Pi AI coding assistant";
-      homepage = "https://github.com/badlogic/pi-mono";
-      license = licenses.mit;
-      platforms = platforms.unix;
-      maintainers = [ ];
-    };
-
-    passthru.updateScript = ./update.sh;
-  };
 }
